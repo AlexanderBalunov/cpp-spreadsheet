97c169b023eab364161f11b566e0f98d5b3f336c
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2025-03-07 17:21:17 +0300

    Update common.h

--- spreadsheet/common.h
+++ spreadsheet/common.h
@@ -39,7 +39,7 @@ public:
     enum class Category {
         Ref,    // ссылка на ячейку с некорректной позицией
         Value,  // ячейка не может быть трактована как число
-        Div0,  // в результате вычисления возникло деление на ноль
+        Arithmetic,  // в результате вычисления возникло деление на ноль
     };
 
     FormulaError(Category category);
@@ -147,3 +147,4 @@ public:
 
 // Создаёт готовую к работе пустую таблицу.
 std::unique_ptr<SheetInterface> CreateSheet();
+
