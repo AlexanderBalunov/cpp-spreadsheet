23f4c11049ab88deebc91275cd8c16136ef30542
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2025-03-07 17:20:26 +0300

    Update cell.h

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -18,19 +18,28 @@ public:
 
     Value GetValue() const override;
     std::string GetText() const override;
+
     std::vector<Position> GetReferencedCells() const override;
 
-    bool IsReferenced() const;
+    bool HasDependentCells() const;
+
+    std::unordered_set<Cell*> GetPrecedentCellsPtrs() const {
+        return precedent_cells;
+    } 
 
 private:
     class Impl;
     class EmptyImpl;
     class TextImpl;
     class FormulaImpl;
-
     std::unique_ptr<Impl> impl_;
 
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
+    bool WillCauseCircularDependency(const Impl& impl_temp) const;
+    void InvalidateCache();
 
+    Sheet& sheet_;
+    // Указатели на ячейки, от которых зависит эта ячейка (для отслеживания кольцевых зависимостей)
+    std::unordered_set<Cell*> precedent_cells;
+    // Указатели на ячейки, которые зависят от этой ячейки (для инвалидации кэша)
+    std::unordered_set<Cell*> dependent_cells;
 };
